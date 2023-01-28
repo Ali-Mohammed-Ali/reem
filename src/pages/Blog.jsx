ec183e493cb85a79b145eba4af9b0f69913cb1d2
Ali-Mohammed-Ali <alimohammed8798@gmail.com>
2023-01-28 20:25:23 +0200

    anything

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,103 @@
 import React from "react";
-function Blog () {
-    return (
-        <div className="blog-container">
-            Blog page
-        </div>
-    )
+import "./scss/blog.scss";
+import blog from "../assets/blog.jpeg";
+import { Link } from "react-router-dom";
+
+function Blog() {
+  return (
+    <div className="blog-container">
+      <h4>Blogs</h4>
+      <div className="blogs">
+        <Link to="/Blog">
+          <div className="blog">
+            <div className="img">
+              <img src={blog} alt="" />
+            </div>
+            <div className="info">
+              <h5>how to be sad</h5>
+              <p>describe how to be sad in ten sec without root...</p>
+            </div>
+          </div>
+        </Link>
+        <Link to="/Blog">
+          <div className="blog">
+            <div className="img">
+              <img src={blog} alt="" />
+            </div>
+            <div className="info">
+              <h5>how to be sad</h5>
+              <p>describe how to be sad in ten sec without root...</p>
+            </div>
+          </div>
+        </Link>
+        <Link to="/Blog">
+          <div className="blog">
+            <div className="img">
+              <img src={blog} alt="" />
+            </div>
+            <div className="info">
+              <h5>how to be sad</h5>
+              <p>describe how to be sad in ten sec without root...</p>
+            </div>
+          </div>
+        </Link>
+        <Link to="/Blog">
+          <div className="blog">
+            <div className="img">
+              <img src={blog} alt="" />
+            </div>
+            <div className="info">
+              <h5>how to be sad</h5>
+              <p>describe how to be sad in ten sec without root...</p>
+            </div>
+          </div>
+        </Link>
+        <Link to="/Blog">
+          <div className="blog">
+            <div className="img">
+              <img src={blog} alt="" />
+            </div>
+            <div className="info">
+              <h5>how to be sad</h5>
+              <p>describe how to be sad in ten sec without root...</p>
+            </div>
+          </div>
+        </Link>
+        <Link to="/Blog">
+          <div className="blog">
+            <div className="img">
+              <img src={blog} alt="" />
+            </div>
+            <div className="info">
+              <h5>how to be sad</h5>
+              <p>describe how to be sad in ten sec without root...</p>
+            </div>
+          </div>
+        </Link>
+        <Link to="/Blog">
+          <div className="blog">
+            <div className="img">
+              <img src={blog} alt="" />
+            </div>
+            <div className="info">
+              <h5>how to be sad</h5>
+              <p>describe how to be sad in ten sec without root...</p>
+            </div>
+          </div>
+        </Link>
+        <Link to="/Blog">
+          <div className="blog">
+            <div className="img">
+              <img src={blog} alt="" />
+            </div>
+            <div className="info">
+              <h5>how to be sad</h5>
+              <p>describe how to be sad in ten sec without root...</p>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 export default Blog;
--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,9 +1,46 @@
 import React from "react";
-function ContactMe () {
-    return (
-        <div className="contactme-container">
-            ContactMe page
+import "./scss/contactme.scss";
+import Cup from "../assets/cup.png";
+function ContactMe() {
+  return (
+    <div className="contactme-container">
+      <h4>Let's Talk</h4>
+      <form>
+        <div className="names">
+          <div className="name">
+            <label htmlFor="firstName">first name</label>
+            <br />
+            <input type="text" id="firstName" required />
+          </div>
+          <div className="name">
+            <label htmlFor="lastName">last name</label>
+            <br></br>
+            <input type="text" id="lastName" required />
+          </div>
         </div>
-    )
+        <div className="email-subject">
+          <div className="email">
+            <label htmlFor="email">email</label>
+            <br />
+            <input type="text" id="email" required />
+          </div>
+          <div className="subject">
+            <label htmlFor="subject">subject</label>
+            <br />
+            <input type="text" id="subject" />
+          </div>
+        </div>
+        <div className="message">
+          <label htmlFor="message">message</label>
+          <br />
+          <textarea type="text" id="message" />
+        </div>
+        <button type="submit">Send</button>
+        <div className="img">
+          <img src={Cup} alt="Let's Talk" />
+        </div>
+      </form>
+    </div>
+  );
 }
 export default ContactMe;
